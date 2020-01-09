633b2c4bc5c47bbbd7e6bbd088f66430e8ff26a3
Lishang-ichigaya <kuromitsu0528@gmail.com>
2020-01-09 20:07:21 +0900

    テスト中

--- main_bsc_2.py
+++ main_bsc_2.py
@@ -17,10 +17,10 @@ from CRC import CRC_Detector
 
 
 if __name__ == '__main__':
-    k = 128
+    k = 256
     r = 0  # CRCの長さを変更する場合はCRC.pyも書き換える
     K = k + r
-    N = 256
+    N = 512
     L = 4
     M = int(np.log2(N))
     chaneltype = "BSC"
@@ -28,24 +28,20 @@ if __name__ == '__main__':
     path = "./sort_I/sort_I_" + str(M) + "_" + str(P) + "_" + "20" + ".dat"
     # path ="./polarcode/"+"sort_I_" + str(M) + "_" + str(P) + "_" + "20" + ".dat"
 
-    kaisu = 10000
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "ber":
-            #kaisu = 500
-
-            for P in [0.06]:
-                # if False:
-                #    with open("bsc_takusannnn.txt",mode='a',encoding='utf-8') as f:
-                #        f.write("-----------------------P="+str(P)+", I(W)="+str(1-BinaryEntropy(P))+"----------------------------\n")
-                #K = 2
-                #N = 4
-                for j in range(1):
+    kaisu = 5500
+    if len(sys.argv) == 2 and sys.argv[1] == "ber":
+        for P in [0.06]:
+            for L in [2, 4, 32]:
+                with open("SCLのテスト2.txt", mode='a', encoding='utf-8') as f:
+                    f.write("-----------------------L="+str(L)+"----------------------------\n")
+                for N in [32, 64, 128, 256]:
+                    k = N//2
+                    K = k + r
                     eroorcount0 = 0
                     frameerrorcout0 = 0
                     eroorcount1 = 0
                     frameerrorcout1 = 0
-                    #K *= 2
-                    #N *= 2
+
                     path = path = "./sort_I/sort_I_" + str(int(np.log2(N))) + "_" + str(P) + "_" + "20" + ".dat"
                     start = time.time()
                     for i in range(kaisu):
@@ -63,55 +59,56 @@ if __name__ == '__main__':
                         bsc.Transmission()
                         output = bsc.output
 
-                        decoder0name = "_SC"
-                        #start0 = time.time()
-                        decoder0 = DecoderW(K, N, output, chaneltype, path, False)
-                        decoder0.DecodeMessage(P)
-                        hat_message0 = Message(K)
-                        hat_message0.message = decoder0.hat_message
-                        #end0 = time.time()
+                        # decoder0name = "_SC"
+                        # start0 = time.time()
+                        # decoder0 = DecoderW(K, N, output, chaneltype, path, False)
+                        # decoder0.DecodeMessage(P)
+                        # hat_message0 = Message(K)
+                        # hat_message0.message = decoder0.hat_message
+                        # end0 = time.time()
 
                         decoder1name = "SCL"
-                        #start1 = time.time()
+                        # start1 = time.time()
                         decoder1 = ListDecoder_F(K, N, L, output, chaneltype, path, False)
                         decoder1.DecodeMessage(P)
                         hat_message1 = Message(k)
                         hat_message1.message = decoder1.hat_message
-                        #end1 = time.time()
+                        # end1 = time.time()
 
-                        error0 = np.bitwise_xor(message.message, hat_message0.message)
+                        # error0 = np.bitwise_xor(message.message, hat_message0.message)
                         error1 = np.bitwise_xor(message.message, hat_message1.message)
 
-                        eroorcount0 += np.count_nonzero(error0)
+                        # eroorcount0 += np.count_nonzero(error0)
                         eroorcount1 += np.count_nonzero(error1)
 
-                        frameerrorcout0 += 0 if np.count_nonzero(error0) == 0 else 1
+                        # frameerrorcout0 += 0 if np.count_nonzero(error0) == 0 else 1
                         frameerrorcout1 += 0 if np.count_nonzero(error1) == 0 else 1
                         print(i, "/", kaisu, "回目, ",
-                                0 if np.count_nonzero(error0) == 0 else 1,
+                              #       0 if np.count_nonzero(error0) == 0 else 1,
                               0 if np.count_nonzero(error1) == 0 else 1)
-                        #print("FSCL:", "{0:.5f}".format(end0-start0), "SCL", "{0:.5f}".format(end1-start1))
+                        # print("FSCL:", "{0:.5f}".format(end0-start0), "SCL", "{0:.5f}".format(end1-start1))
                     end = time.time()
 
-                    if False:
-                        with open("bsc_scltakusan.txt", mode='a', encoding='utf-8') as f:
-                            f.write("K="+str(K)+", N=" +
-                                    str(N)+", P="+str(P)+"\n")
-                            f.write("送信: " + str(K*kaisu) + ", LR復号誤り: " + str(eroorcount1)+"\n")
-                            f.write("FER__W: " + str(frameerrorcout0/kaisu)+"\n")
-                            f.write("FER_LR: " + str(frameerrorcout1/kaisu)+"\n")
-                            f.write("BER__W: " + str(eroorcount0/(K*kaisu))+"\n")
-                            f.write("BER_LR: " + str(eroorcount1/(K*kaisu))+"\n")
+                    if True:
+                        with open("SCLのテスト2.txt", mode='a', encoding='utf-8') as f:
+                            f.write("K="+str(K)+", N="+str(N) + ", r=" + str(r) + ", L="+str(L)+", P="+str(P)+"\n")
+                            # f.write("送信メッセージ数: " + str(K*kaisu)+", " + decoder0name + "復号誤り: "
+                            #       + str(eroorcount0)+", " + decoder1name + "復号誤り: " + str(eroorcount1))
+                            f.write("送信メッセージ数: " + str(K*kaisu)+", " + decoder1name + "復号誤り: " + str(eroorcount1)+"\n")
+                            #f.write("FER_" + decoder0name + ": " + str(frameerrorcout0/kaisu)+"\n")
+                            f.write("FER_" + decoder1name + ": " + str(frameerrorcout1/kaisu)+"\n")
+                            #f.write("BER_" + decoder0name + ": " + str(eroorcount0/(K*kaisu))+"\n")
+                            f.write("BER_" + decoder1name + ": " + str(eroorcount1/(K*kaisu))+"\n")
                             f.write("実行時間: " + str(end-start)+"\n")
 
                     if True:
                         print("K="+str(K)+", N="+str(N) + ", r=" + str(r) + ", L="+str(L)+", P="+str(P))
-                        print("送信メッセージ数: " + str(K*kaisu)+", " + decoder0name + "復号誤り: " +
-                              str(eroorcount0)+", " + decoder1name + "復号誤り: " + str(eroorcount1))
-                        #print("送信メッセージ数: " + str(K*kaisu)+", " + decoder1name + "復号誤り: " + str(eroorcount1))
-                        print("FER_" + decoder0name + ": " + str(frameerrorcout0/kaisu))
+                        #print("送信メッセージ数: " + str(K*kaisu)+", " + decoder0name + "復号誤り: " +
+                        #      str(eroorcount0)+", " + decoder1name + "復号誤り: " + str(eroorcount1))
+                        print("送信メッセージ数: " + str(K*kaisu)+", " + decoder1name + "復号誤り: " + str(eroorcount1))
+                        #print("FER_" + decoder0name + ": " + str(frameerrorcout0/kaisu))
                         print("FER_" + decoder1name + ": " + str(frameerrorcout1/kaisu))
-                        print("BER_" + decoder0name + ": " + str(eroorcount0/(K*kaisu)))
+                        #print("BER_" + decoder0name + ": " + str(eroorcount0/(K*kaisu)))
                         print("BER_" + decoder1name + ": " + str(eroorcount1/(K*kaisu)))
                         print("実行時間: " + str(end-start))
 
@@ -136,11 +133,11 @@ if __name__ == '__main__':
         output = bsc.output
         print("通信路出力:\t\t", output)
 
-        #decoder0 = ListDecoder_F(K, N, L, output, chaneltype, path, False)
+        # decoder0 = ListDecoder_F(K, N, L, output, chaneltype, path, False)
         # decoder0.DecodeMessage(P)
-        #hat_message0 = Message(K)
-        #hat_message0.message = decoder0.hat_message
-        #print("  SCLメッセージ推定値:\t", hat_message1.message)
+        # hat_message0 = Message(K)
+        # hat_message0.message = decoder0.hat_message
+        # print("  SCLメッセージ推定値:\t", hat_message1.message)
 
         decoder1 = ListDecoder_CRC(K, N, L, output, chaneltype, path, False)
         decoder1.DecodeMessage(P)
@@ -148,9 +145,9 @@ if __name__ == '__main__':
         hat_message1.message = decoder1.hat_message[:k]
         print("CASCLメッセージ推定値:\t", hat_message1.message)
 
-        #print("本当のメッセージ:\t", message.message)
+        # print("本当のメッセージ:\t", message.message)
 
-        #error0 = np.bitwise_xor(message.message, hat_message0.message)
-        #print("  SCL誤り数:", np.count_nonzero(error0))
+        # error0 = np.bitwise_xor(message.message, hat_message0.message)
+        # print("  SCL誤り数:", np.count_nonzero(error0))
         error1 = np.bitwise_xor(message.message, hat_message1.message)
         print("CASCL誤り数:", np.count_nonzero(error1))
