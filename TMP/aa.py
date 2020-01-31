02d92063032c3e942092db5ba8ca54d9d1456a37
Lishang-ichigaya <kuromitsu0528@gmail.com>
2020-01-31 15:49:56 +0900

    512もできるようにした

--- TMP/aa.py
+++ TMP/aa.py
@@ -9,15 +9,16 @@ def GetInformationIndex(K, path):
     return np.sort(informationindex[:K])
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("aa.py P r k\nP:反転確率 r:CRC長 k:メッセージ長")
+    if len(sys.argv) != 5:
+        print("aa.py m P r k\nm:符号長のlog2 P:反転確率 r:CRC長 k:メッセージ長")
         exit()
 
-    P = float(sys.argv[1])
-    r = int(sys.argv[2])
-    k = int(sys.argv[3])
-    infindex = GetInformationIndex(k+r, "../sort_I/sort_I_8_"+str(P)+"_20.dat")
-    IW_n = np.loadtxt("IW"+sys.argv[1][1:]+".txt")
+    m = int(sys.argv[1])
+    P = float(sys.argv[2])
+    r = int(sys.argv[3])
+    k = int(sys.argv[4])
+    infindex = GetInformationIndex(k+r, "../sort_I/sort_I_"+str(m)+"_"+str(P)+"_20.dat")
+    IW_n = np.loadtxt(str(2**m)+"_IW"+sys.argv[2][1:]+".txt")
 
     #print(sum(IW_n[infindex[:threshold]]))
     #print(sum(IW_n[infindex[threshold:]]))
